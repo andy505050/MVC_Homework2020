2b0a37781b19ad1264f6e82fbe82de401aed7480
SadGuy <alex60510@hotmail.com>
2020-10-30 02:11:54 +0800

    實作「客戶聯絡人」時，同一個客戶下的聯絡人，其 Email 不能重複

--- MVC_Homework2020/Models/客戶聯絡人.Partial.cs
+++ MVC_Homework2020/Models/客戶聯絡人.Partial.cs
@@ -1,3 +1,6 @@
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+
 namespace MVC_Homework2020.Models
 {
     using System;
@@ -5,8 +8,16 @@ namespace MVC_Homework2020.Models
     using System.ComponentModel.DataAnnotations;
 
     [MetadataType(typeof(客戶聯絡人MetaData))]
-    public partial class 客戶聯絡人
+    public partial class 客戶聯絡人 : IValidatableObject
     {
+        private 客戶資料Entities db = new 客戶資料Entities();
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (db.客戶聯絡人.Any(x => x.Id != Id && x.Email == Email && x.是否已刪除 == false))
+            {
+                yield return new ValidationResult("同一個客戶下的聯絡人，其 Email 不能重複", new string[] { "Email" });
+            }
+        }
     }
 
     public partial class 客戶聯絡人MetaData
@@ -16,23 +27,23 @@ namespace MVC_Homework2020.Models
         [Required]
         public int 客戶Id { get; set; }
 
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         [Required]
         public string 職稱 { get; set; }
 
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         [Required]
         public string 姓名 { get; set; }
 
-        [StringLength(250, ErrorMessage="欄位長度不得大於 250 個字元")]
+        [StringLength(250, ErrorMessage = "欄位長度不得大於 250 個字元")]
         [Required]
         [EmailAddress]
         public string Email { get; set; }
 
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         public string 手機 { get; set; }
 
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         public string 電話 { get; set; }
 
         public virtual 客戶資料 客戶資料 { get; set; }
