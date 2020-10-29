630e0755a63a1ee9c0a38d9a9b4bd1cbc817e262
SadGuy <alex60510@hotmail.com>
2020-10-30 01:35:50 +0800

    實作一個清單頁面，顯示欄位有「客戶名稱、聯絡人數量、銀行帳戶數量」共三個欄位，此功能要在資料庫中建立一個檢視表，並且匯入 EDMX。

---
+++ MVC_Homework2020/Controllers/客戶檢視Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Homework2020.Models;
+
+namespace MVC_Homework2020.Controllers
+{
+    public class 客戶檢視Controller : Controller
+    {
+        // GET: 客戶檢視
+        private 客戶資料Entities db = new 客戶資料Entities();
+        public ActionResult Index()
+        {
+            var data = db.客戶檢視;
+            return View(data);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
