f12cde53e17a0ac808024575baf090b58052eb8b
SadGuy <alex60510@hotmail.com>
2020-10-29 23:23:06 +0800

    客戶資料查詢 : R

---
+++ MVC_Homework2020/Controllers/客戶資料Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Homework2020.Models;
+
+namespace MVC_Homework2020.Controllers
+{
+    public class 客戶資料Controller : Controller
+    {
+        客戶資料Entities db = new 客戶資料Entities();
+        // GET: 客戶資料
+        public ActionResult Index(string 搜尋客戶名稱)
+        {
+            var data = db.客戶資料.AsQueryable();
+
+            if (!string.IsNullOrEmpty(搜尋客戶名稱))
+            {
+                data = data.Where(x => x.客戶名稱.Contains(搜尋客戶名稱));
+                ViewBag.搜尋客戶名稱 = 搜尋客戶名稱;
+            }
+
+            return View(data);
+        }
+    }
+}
