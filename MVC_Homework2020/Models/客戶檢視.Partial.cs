8a25ace675808b7bdf46227735e300873d7eacb1
SadGuy <alex60510@hotmail.com>
2020-10-30 01:41:52 +0800

    使用 EFGeneratePartialClass.tt 產稱Partial Class

---
+++ MVC_Homework2020/Models/客戶檢視.Partial.cs
@@ -0,0 +1,23 @@
+namespace MVC_Homework2020.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    [MetadataType(typeof(客戶檢視MetaData))]
+    public partial class 客戶檢視
+    {
+    }
+    
+    public partial class 客戶檢視MetaData
+    {
+        [Required]
+        public int Id { get; set; }
+        
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [Required]
+        public string 客戶名稱 { get; set; }
+        public Nullable<int> 聯絡人數量 { get; set; }
+        public Nullable<int> 銀行帳戶數量 { get; set; }
+    }
+}
