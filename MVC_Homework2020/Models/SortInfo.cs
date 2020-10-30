1d1acab23981fb8eeed034f821a6eed6148df8d0
SadGuy <alex60510@hotmail.com>
2020-10-31 00:24:30 +0800

    建立記錄排序狀態的類別 & 安裝 Linq Dynamic 套件

---
+++ MVC_Homework2020/Models/SortInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Homework2020.Models
+{
+    public class SortInfo
+    {
+        public string CurrentSortCol { get; set; }
+        public string OriSortCol { get; set; }
+        public string SortDirection { get; set; }
+    }
+}
