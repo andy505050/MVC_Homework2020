f4475b2cee2c33cbb2cbbd775fffd3c85dbb89c5
SadGuy <alex60510@hotmail.com>
2020-10-31 01:32:39 +0800

    客戶資料匯出

--- MVC_Homework2020/Controllers/客戶資料Controller.cs
+++ MVC_Homework2020/Controllers/客戶資料Controller.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using ClosedXML.Excel;
 using MVC_Homework2020.Models;
 using Omu.ValueInjecter.Utils;
 
@@ -18,9 +20,14 @@ namespace MVC_Homework2020.Controllers
         }
 
         // GET: 客戶資料
-        public ActionResult Index(string 搜尋客戶名稱, string 客戶分類, SortInfo sortInfo)
+        public ActionResult Index(string 搜尋客戶名稱, string 客戶分類, SortInfo sortInfo, string submitBtn)
         {
             var data = repo.Filter(搜尋客戶名稱, 客戶分類);
+            if (submitBtn == "export")
+            {
+                return Export(data);
+            }
+
             data = repo.OrderBy(data, sortInfo);
 
             ViewBag.搜尋客戶名稱 = 搜尋客戶名稱;
@@ -28,6 +35,23 @@ namespace MVC_Homework2020.Controllers
             ViewBag.SortDirection = sortInfo.SortDirection == "asc" ? "desc" : "asc";
             ViewBag.OriSortCol = sortInfo.CurrentSortCol;
             return View(data);
+        }
+
+        public ActionResult Export(IEnumerable<客戶資料> 客戶清單)
+        {
+
+            using (XLWorkbook wb = new XLWorkbook())
+            {
+                var data = 客戶清單.Select(x => new { x.客戶名稱, x.客戶分類, x.電話, x.地址 });
+                var ws = wb.Worksheets.Add("客戶資料");
+                ws.Cell(1, 1).Value = data;
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    wb.SaveAs(memoryStream);
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+                    return File(memoryStream.ToArray(), "application/vnd.ms-excel", "Download.xlsx");
+                }
+            }
         }
 
         public ActionResult Create()
