457f07640af809a3a787c3c88b712ed3a56310bf
SadGuy <alex60510@hotmail.com>
2020-10-31 01:35:50 +0800

    客戶檢視錯誤修正

--- MVC_Homework2020/Models/客戶檢視.Partial.cs
+++ MVC_Homework2020/Models/客戶檢視.Partial.cs
@@ -3,17 +3,17 @@ namespace MVC_Homework2020.Models
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    
+
     [MetadataType(typeof(客戶檢視MetaData))]
     public partial class 客戶檢視
     {
     }
-    
+
     public partial class 客戶檢視MetaData
     {
         [Required]
-        public int Id { get; set; }
-        
+        public int ID { get; set; }
+
         [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
         [Required]
         public string 客戶名稱 { get; set; }
